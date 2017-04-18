01b9021738d47631cb73ee0367dbabd6ad5463d0
Michel Agopian <mishuagopian@gmail.com>
2017-04-18 09:36:35 -0300

    adding comments in spy class

--- spec/spy.rb
+++ spec/spy.rb
@@ -1,4 +1,22 @@
+#
+# Class used in tests to keep track of amount of calls for a method in a klass.
+#
+# Example in a controller spec:
+#
+# before do
+#   Spy.spy(controller, 'authenticate_entity')
+#   perform_some_request
+# end
+#
+# it 'calls authenticate_entity defined by the user' do
+#   count = controller.spied_authenticate_entity_counter
+#   expect(count).to be(1)
+# end
+#
 class Spy
+
+  # Defines a counter for the given method and overrides it to update
+  # the counter in every call before being executed.
   def self.spy(klass, method)
     define_counter(klass, method)
     klass.singleton_class.class_eval do
@@ -9,6 +27,7 @@ class Spy
     end
   end
 
+  # Defines a counter (and its getter) for the given method as an instance variable of the given klass.
   def self.define_counter(klass, method)
     klass.instance_variable_set("@spied_#{method}_counter".to_sym, 0)
 
@@ -18,4 +37,5 @@ class Spy
       end
     end
   end
+
 end
