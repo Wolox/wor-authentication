25df7c60b6132f0854f0ba5f674a8ddfe2c3f2c4
Michel Agopian <mishuagopian@gmail.com>
2017-05-04 20:14:51 -0300

    testing exception raise on invalid initializer configuration

--- spec/wor/authentication_spec.rb
+++ spec/wor/authentication_spec.rb
@@ -29,25 +29,41 @@ describe Wor::Authentication do
     let(:new_expiration_days) { 5 }
     let(:new_maximum_useful_days) { 30 }
 
-    before do
-      described_class.configure do |config|
-        config.expiration_days = new_expiration_days
-        config.maximum_useful_days = new_maximum_useful_days
+    context 'when setting valid data' do
+      before do
+        described_class.configure do |config|
+          config.expiration_days = new_expiration_days
+          config.maximum_useful_days = new_maximum_useful_days
+        end
+      end
+
+      it 'can configure new_expiration_days' do
+        expect(described_class.config[:expiration_days]).to eq(new_expiration_days)
+      end
+
+      it 'can configure maximum_useful_days' do
+        expect(described_class.config[:maximum_useful_days]).to eq(new_maximum_useful_days)
+      end
+
+      after do
+        described_class.configure do |config|
+          config.expiration_days = default_expiration_days
+          config.maximum_useful_days = default_maximum_useful_days
+        end
       end
     end
 
-    it 'can configure new_expiration_days' do
-      expect(described_class.config[:expiration_days]).to eq(new_expiration_days)
-    end
+    context 'when setting valid data' do
+      it 'raises exception when setting an invalid expiration_days value' do
+        expect {
+          described_class.expiration_days = 'invalid-expiration_days-value'
+        }.to raise_error(Wor::Authentication::Exceptions::InvalidExpirationDaysError)
+      end
 
-    it 'can configure maximum_useful_days' do
-      expect(described_class.config[:maximum_useful_days]).to eq(new_maximum_useful_days)
-    end
-
-    after do
-      described_class.configure do |config|
-        config.expiration_days = default_expiration_days
-        config.maximum_useful_days = default_maximum_useful_days
+      it 'raises exception when setting an invalid maximum_useful_days value' do
+        expect {
+          described_class.maximum_useful_days = 'invalid-maximum_useful_days-value'
+        }.to raise_error(Wor::Authentication::Exceptions::InvalidMaximumUsefulDaysError)
       end
     end
   end
