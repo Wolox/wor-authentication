a6ea158e1d3de08f3f0c28fc3faceaa17500426a
Leandro <leamotta@msn.com>
2017-09-04 12:01:12 -0300

    add encoding algorithm, jwt fixed version (2.0)

--- lib/wor/authentication/token_manager.rb
+++ lib/wor/authentication/token_manager.rb
@@ -3,16 +3,18 @@ require 'jwt'
 module Wor
   module Authentication
     class TokenManager
+      ENCODING_ALGORITHM = 'HS256'.freeze
+
       def initialize(key)
         @key = key
       end
 
       def encode(payload)
-        JWT.encode(payload, @key)
+        JWT.encode(payload, @key, ENCODING_ALGORITHM)
       end
 
       def decode(token)
-        payload = JWT.decode(token, @key)[0]
+        payload = JWT.decode(token, @key, true, algorithm: ENCODING_ALGORITHM)[0]
         Wor::Authentication::DecodedToken.new(payload)
       rescue
         raise Wor::Authentication::Exceptions::InvalidAuthorizationToken
